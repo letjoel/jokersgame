4b4db941628f2e110cc82542761727dc174ba450
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2022-11-24 13:15:27 +0100

    aded audio functions and new audio.js

---
+++ JS/audio.js
@@ -0,0 +1,18 @@
+export function playerDead(){
+const playerdead= new Audio("/Assets/fx/knifeslice.mp3")
+    playerdead.play()
+}
+
+export function rollDice(){
+    const rolldice=new Audio("/Assets/fx/dicerolling.mp3")
+    rolldice.play()
+}
+export function knifeSlice(){
+    const knifeslice=new Audio("/Assets/fx/knifeslice.mp3")
+    knifeslice.play()
+}
+export function screams(){
+    const scream2=new Audio("/Assets/fx/scream2.mp3")
+    scream2.play()
+}
+
