2abb0d531e12b3d2469f4c2c6e5e11b1b7227094
ManuKovaSG <kovacicmanu@gmail.com>
2022-11-24 12:02:42 +0100

    cambio SOLO en comentario main__container

---
+++ JS/fase3.js
@@ -0,0 +1,14 @@
+/*SelectCoder(name): Creates method that modifies a variable SelectedCoder with the selected name and make some visible render that highlight the avatar*/
+
+1 debe haber una variable selectedCoder
+2 la variable debe ser modificada por un Input que provendra de FASE 1
+
+
+
+
+
+var selectedCoder = newName
+
+function newName(p){
+
+}
