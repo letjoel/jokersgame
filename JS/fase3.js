136057c71e61217896cd4ff096c112d10563e891
ManuKovaSG <kovacicmanu@gmail.com>
2022-11-28 02:04:36 +0100

    intento de javascript

--- JS/fase3.js
+++ JS/fase3.js
@@ -1,7 +1,12 @@
 /*SelectCoder(name): Creates method that modifies a variable SelectedCoder with the selected name and make some visible render that highlight the avatar*/
-
+/* 
 1 debe haber una variable selectedCoder
 2 la variable debe ser modificada por un Input que provendra de FASE 1
+3 con el metodo Push tendre que ir agregando al nuevo player a la lista
+4 tiene que haber un array donde esten los "div" de jugadores predeterminados y se pueda ir pusheando los q ingrese el usuario
+
+
+*/
 
 
 
@@ -10,5 +15,23 @@
 var selectedCoder = newName
 
 function newName(p){
-
+    document.getElementsByClassName(main__first__input)
 }
+
+/* use the each() function of jQuery and iterate over each div to get the id and push it into an array.
+
+var $array = [];
+$('div').each(function(){
+    var id = $(this).attr('id');
+    $array.push(id);
+});
+
+/* para el boton quitar el elemento, podria utilizar un pop():
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+// → [1, 2, 3, 4, 5]
+console.log(sequence.pop());
+// → 5
+ */
