a579e6bdb3ed5e5895b4ec4bba6d4a61b5bfa0e5
Joel Herrero <joelv2@gmail.com>
2022-11-29 19:18:19 +0100

    cleaning main to merge easyly

--- JS/navmobile.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.querySelector('#header__hamburger');
-const navAside = document.querySelector('.nav__aside');
-
-
-hamburguer.addEventListener('click', ()=> {
-    navAside.classList.toggle('nav__aside--display');
-})
